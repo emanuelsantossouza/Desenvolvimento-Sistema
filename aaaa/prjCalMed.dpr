0372218c54eff0ff68d25644930430026683b009
Emanuel Santos Souza de Jesus <emanuelsantoss42@gmail.com>
2022-09-20 22:29:36 -0300

    20-09

---
+++ aaaa/prjCalMed.dpr
@@ -0,0 +1,14 @@
+program prjCalMed;
+
+uses
+  Vcl.Forms,
+  untCalMed in 'untCalMed.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
